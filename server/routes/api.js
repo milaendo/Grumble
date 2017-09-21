2375039e1c9e99b39804f1faefe16b4d92669e09
Mila Endo <milal.alim@gmail.com>
2017-09-21 16:01:12 -0700

    created endpoint for creating a grumb

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,9 +7,11 @@ const conn = mysql.createConnection({
   host: config.get('db.host'),
   database: config.get('db.database'),
   user: config.get('db.user'),
-  password: config.get('db.password')
+  password: config.get('db.password'),
 })
-
+conn.on("error",function(err){
+	console.log(err)
+})
 /* /api starting endpoint */
 router.post('/register', function(req, res, next) {
 	const user_name = req.body.username
@@ -19,6 +21,7 @@ router.post('/register', function(req, res, next) {
 	const sql='INSERT INTO users (user_name, password, display_name) values (?, ?, ?);'
 	conn.query(sql,[user_name, password, display_name],function(err,results,fields){
 		if (err){
+			console.log(err)
 			res.json({
 				message:'user info not inserted'
 			})
@@ -32,6 +35,24 @@ router.post('/register', function(req, res, next) {
 	})
 });
 
+router.post('/grumb', function(req,res,next){
+	const grumb = req.body.grumb
+	const userid = req.params.id
 
-
+	const sql = 'insert into grumbs (grumb,userid) values (?,?)'
+	conn.query(sql,[grumb,userid], function(err,results,fields){
+		if (err){
+			console.log(err)
+			res.json({
+				message:"grumb was not created"
+			})
+		}
+		else {
+			console.log("grumb created", results)
+			res.json({
+				message:"grumb created successfully"
+			})
+		}
+	})
+})
 module.exports = router;
