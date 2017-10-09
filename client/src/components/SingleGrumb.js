0b6c01c8d5481c7e5b162ebcda674292cdf6f5c0
Mila Endo <milal.alim@gmail.com>
2017-10-09 10:24:52 -0700

    minor changes

--- client/src/components/SingleGrumb.js
+++ client/src/components/SingleGrumb.js
@@ -13,8 +13,9 @@ class SingleGrumb extends Component {
 	render () {
 		return (this.props.active ?
 			<Comment className="singleGrumbBox">
-					<Comment.Text as='h1' className="singleGrumb">{this.props.grumb}</Comment.Text>
-					<Comment.Metadata>Grumbled by: {this.props.display_name} {moment(this.props.timestamp).format('MMM Do YYYY')}</Comment.Metadata>
+				<Comment.Metadata className='largerFont'>{this.props.display_name} says:</Comment.Metadata>
+				<Comment.Text as='h2' className="singleGrumb">{this.props.grumb}</Comment.Text>
+				<Comment.Metadata className='largerFont'>{moment(this.props.timestamp).format('MMM Do YYYY')}</Comment.Metadata>
 			</Comment> 	:
 			<Comment className="singleGrumbBox">
 					<Comment.Text as='h1' className="singleGrumb">This grumb is no more!!</Comment.Text>
